d8b5db9a64a494b7256bc582ff9a1c0866342a6e
Kat Perez <xylene.dev@gmail.com>
2024-06-28 19:58:24 -0400

    add "hello-world" serverless function

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const recordBtn = document.getElementById("record-btn")
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+recordBtn?.addEventListener("click", () => {
+  console.log("clicked the button")
+  openAiTranscription()
+})
+
+async function openAiTranscription() {
+  const response = await fetch("/.netlify/functions/openai-whisper").then(response => response.json())
+  console.log(JSON.stringify(response))
+}
